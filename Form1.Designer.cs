19f929f69b08ee58db863fb3b2acc7c196b12b04
Vamic <made.swede@live.com>
2014-09-07 22:59:24 +0200

    wtf am i doing

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace FloatingChair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.exitImage = new System.Windows.Forms.PictureBox();
+            this.browserButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.exitImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 35);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Visible = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(113, 35);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 4;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Visible = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 50);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Press \'Open Browser\'.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.75F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.MinimumSize = new System.Drawing.Size(195, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "          Anna";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // exitImage
+            // 
+            this.exitImage.BackColor = System.Drawing.Color.Transparent;
+            this.exitImage.Cursor = System.Windows.Forms.Cursors.Default;
+            this.exitImage.ErrorImage = null;
+            this.exitImage.Image = ((System.Drawing.Image)(resources.GetObject("exitImage.Image")));
+            this.exitImage.InitialImage = ((System.Drawing.Image)(resources.GetObject("exitImage.InitialImage")));
+            this.exitImage.Location = new System.Drawing.Point(176, 8);
+            this.exitImage.Name = "exitImage";
+            this.exitImage.Size = new System.Drawing.Size(16, 16);
+            this.exitImage.TabIndex = 8;
+            this.exitImage.TabStop = false;
+            this.exitImage.Click += new System.EventHandler(this.exitImage_Click);
+            // 
+            // browserButton
+            // 
+            this.browserButton.Location = new System.Drawing.Point(12, 35);
+            this.browserButton.Name = "browserButton";
+            this.browserButton.Size = new System.Drawing.Size(176, 23);
+            this.browserButton.TabIndex = 9;
+            this.browserButton.Text = "Open Browser";
+            this.browserButton.UseVisualStyleBackColor = true;
+            this.browserButton.Click += new System.EventHandler(this.browserButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(23, 26);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.ClientSize = new System.Drawing.Size(200, 125);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.browserButton);
+            this.Controls.Add(this.exitImage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Chair";
+            ((System.ComponentModel.ISupportInitialize)(this.exitImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.PictureBox exitImage;
+        private System.Windows.Forms.Button browserButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+// Requires reference to WebDriver.Support.dll
+using OpenQA.Selenium.Support.UI;
+using System.Timers;
+using System.Threading;
+using OpenQA.Selenium.Remote;
+namespace FloatingChair
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer aTimer;
+        IWebDriver driver;
+        Random rng = new Random();
+        bool ready;
+        int gameState = 0;
+        /* 0 = Game lobby, not started
+         * 1 = Started, not our turn
+         * 2 = Our turn! Do shit, hurry hurry!
+         * 3 = Game is over.
+         */
+        public Form1()
+        {
+            InitializeComponent();
+            
+            /*Events for dragging the window.*/
+            label1.MouseDown += new MouseEventHandler(label1_MouseDown);
+            label1.MouseUp += new MouseEventHandler(label1_MouseUp);
+            label1.MouseMove += new MouseEventHandler(label1_MouseMove);
+            /*The bot does things whenever the timer reaches the set time*/
+            aTimer = new System.Timers.Timer(50);
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+        }
+        /*Safely changes the text of the textbox from different threads. AKA it doesn't crash.*/
+        public void SetTextBox(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(SetTextBox), new object[] { value });
+                return;
+            }
+            textBox1.Text = value;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            ready = true;
+            aTimer.Start();
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            if (ready)
+            {
+                /*                                                                          *
+                 * Checks if the game has started by looking at the continueButton class.   *
+                 * If the button exists the game has started.                               *  
+                 * If the button exists and is clickable its our turn to click.             *
+                 *                                                                          */
+                IList<IWebElement> continueButton = driver.FindElements(By.ClassName("continueButton"));
+                if (continueButton.Count > 0)
+                {
+                    if (gameState != 1 && !continueButton[0].Enabled)
+                        gameState = 1;
+                    else if (gameState != 2 && continueButton[0].Enabled)
+                        gameState = 2;
+                }
+                else if (gameState != 3 && driver.FindElements(By.ClassName("eventTitle"))[0].Text.ToLower().EndsWith(" won!"))
+                    gameState = 3;
+                DoStuff();
+            }
+        }
+        /*                                          *
+         *  This is where the bot does everything.  *
+         *                                          */
+        private void DoStuff()
+        {
+            ready = false;
+            #region Lobby Actions
+            if (gameState == 0)
+            {
+                if (textBox1.Text != "Waiting for game to start...")
+                {
+                    SetTextBox("Waiting for game to start...");
+                }
+                ready = true;
+                return;
+            }
+#endregion
+            #region Opponent's Turn Actions
+            else if (gameState == 1)
+            {
+                /*
+                 TODO: Put in options for using items and skills in inventory.
+                 */
+                if (textBox1.Text != "Waiting for opponent...")
+                {
+                    SetTextBox("Waiting for opponent...");
+                }
+                ready = true;
+                return;
+            }
+            #endregion
+            #region Our Turn Actions
+            else if (gameState == 2)
+            {
+                if (textBox1.Text != "Playing turn...")
+                {
+                    SetTextBox("Playing turn...");
+                }
+                IList<IWebElement> continueButton = driver.FindElements(By.ClassName("continueButton"));
+                IList<IWebElement> playerSelectButton = driver.FindElements(By.ClassName("playerSelectButton"));
+                IList<IWebElement> itemSelectButton = driver.FindElements(By.ClassName("itemSelectButton"));
+                IList<IWebElement> numberInput = driver.FindElements(By.ClassName("numberInput"));
+                IList<IWebElement> eventTitle = driver.FindElements(By.ClassName("eventTitle"));
+                #region Clicking actions
+                if (continueButton.Count == 0){}
+                #region Click button 1
+                else if (continueButton[0].Text == "Continue" ||
+                    continueButton[0].Text == "Roll Dice" ||
+                    continueButton[0].Text == "Roll streak x2" ||
+                    continueButton[0].Text == "Roll streak x3" ||
+                    continueButton[0].Text == "Proceed..." ||
+                    continueButton[0].Text == "Let's go" ||
+                    continueButton[0].Text == "Yes" ||
+                    continueButton[0].Text == "Yes!" ||
+                    continueButton[0].Text == "Go in!" ||
+                    continueButton[0].Text == "I'll join you!" ||
+                    continueButton[0].Text == "Awesome!" ||
+                    continueButton[0].Text == "Deal!" ||
+                    continueButton[0].Text == "Mathematical!" ||
+                    continueButton[0].Text == "Aim it forward." ||
+                    continueButton[0].Text == "I'll launch myself!" ||
+                    continueButton[0].Text == "Going down!" ||
+                    continueButton[0].Text == "Charge items with spells" ||
+                    continueButton[0].Text == "Hmmm... okay.")
+                {
+                    continueButton[0].Click();
+                    ready = true;
+                    return;
+                }
+                #endregion
+                #region Click button 2
+                else if (continueButton[0].Text == "Go in blindly." ||
+                         continueButton[0].Text == "Go Forward!") //gandalf
+                {
+                    continueButton[1].Click();
+                    ready = true;
+                    return;
+                }
+                #endregion
+                #region Click button 1 or 2
+                else if (continueButton[0].Text == "Cowboys" ||
+                         continueButton[0].Text == "Ya rly!")
+                {
+                    continueButton[rng.Next(0, 2)].Click();
+                    ready = true;
+                    return;
+                }
+                #endregion
+                #region Click any button
+                else if (continueButton[0].FindElements(By.XPath("*")).Count > 0 ||//For when items are given out
+                         continueButton[0].Text == "Ask the bartender for a drink." ||
+                         continueButton[0].Text == "The Church." ||
+                         eventTitle[0].Text == "Crossroads" ||
+                         eventTitle[0].Text == "Church" ||
+                         eventTitle[0].Text == "Reference Time!" ||
+                         eventTitle[0].Text == "Dungeon Entrance" ||
+                         eventTitle[0].Text == "Ooo" ||
+                         eventTitle[0].Text == "Middle Ages" ||
+                         eventTitle[0].Text == "Specialized Adventuring!" ||
+                         continueButton[0].Text == "Pick a random option")
+                {
+                    continueButton[rng.Next(0, continueButton.Count())].Click();
+                    ready = true;
+                    return;
+                }
+                #endregion
+                #region Click any button but the last one.
+                else if (continueButton[0].Text == "Play" ||
+                         continueButton[0].Text == "Buy" ||
+                         eventTitle[0].Text == "Dark Wand" ||
+                         eventTitle[0].Text == "Light Wand" ||
+                         eventTitle[0].Text == "Staff" ||
+                         eventTitle[0].Text == "Orb")
+                {
+                    continueButton[rng.Next(0, continueButton.Count() - 1)].Click();
+                    ready = true;
+                    return;
+                }
+                #endregion
+                
+#endregion
+                #region Non-clicking Actions
+                if (playerSelectButton.Count == 0){}
+                else
+                {
+                    IList<IWebElement> radioButton = driver.FindElements(By.ClassName("playerSelectInput"));
+                    radioButton[rng.Next(0, radioButton.Count)].Click();
+                    playerSelectButton[0].Click();
+                    ready = true;
+                    return;
+                }
+                if (itemSelectButton.Count == 0) { }
+                else
+                {
+                    itemSelectButton[rng.Next(0, itemSelectButton.Count())].Click();
+                    ready = true;
+                    return;
+                }
+                if (numberInput.Count == 0) { }
+                else
+                {
+                    numberInput[0].SendKeys(""+rng.Next(0,21));
+                    driver.FindElement(By.ClassName("numberInputButton")).Click();
+                    ready = true;
+                    return;
+                }
+                #endregion
+                //Last Resort.  When playing with people having this active is recommended.
+                if (continueButton.Count != 0)
+                {
+                    //continueButton[rng.Next(0, continueButton.Count())].Click();
+                }
+                ready = true;
+                return;
+            }
+            #endregion
+            #region Game Over Actions
+            else if (gameState == 3)
+            {
+                if (textBox1.Text != "Game over, naviate to a new game and press 'Start'.")
+                {
+                    aTimer.Stop();
+                    SetTextBox("Game over, naviate to a new game and press 'Start'.");
+                }
+                ready = true;
+                return;
+            }
+            #endregion
+            else
+            {
+                ready = true;
+                return;
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            aTimer.Stop();
+        }
+#region Stolen dragging code from SO
+        private bool _Moving = false;
+        private Point _Offset;
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _Moving = true;
+            _Offset = new Point(e.X, e.Y);
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_Moving)
+            {
+                Point newlocation = this.Location;
+                newlocation.X += e.X - _Offset.X;
+                newlocation.Y += e.Y - _Offset.Y;
+                this.Location = newlocation;
+            }
+        }
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (_Moving)
+            {
+                _Moving = false;
+            }
+        }
+#endregion
+        private void exitImage_Click(object sender, EventArgs e)
+        {
+            aTimer.Stop();
+            if (driver!=null)
+                driver.Quit();
+            this.Close();
+        }
+
+        private void browserButton_Click(object sender, EventArgs e)
+        {
+            SetTextBox("Loading...");
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.boardgame-online.com");
+            startButton.Visible = true;
+            stopButton.Visible = true;
+            browserButton.Visible = false;
+            SetTextBox("Navigate to the game lobby and press 'Start' whenever.");
+        }
+    }
+}
